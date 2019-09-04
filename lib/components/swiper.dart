8d93998a26f7a5c5be88a267c739359cb7f34e06
Deeper-Lee <984130957@qq.com>
2019-09-04 14:06:13 +0800

    添加了DIO请求和轮播图swiper

---
+++ lib/components/swiper.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class SwiperView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _SwiperViewState();
+  }
+}
+class _SwiperViewState extends State<SwiperView> {
+  List<Widget> imageList = [
+    Image.network('https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2726034926,4129010873&fm=26&gp=0.jpg', fit: BoxFit.cover,),
+    Image.network('https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2726034926,4129010873&fm=26&gp=0.jpg', fit: BoxFit.cover,),
+    Image.network('https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2726034926,4129010873&fm=26&gp=0.jpg', fit: BoxFit.cover,),
+    Image.network('https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2726034926,4129010873&fm=26&gp=0.jpg', fit: BoxFit.cover,),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return firstSwiperView();
+  }
+
+  Widget firstSwiperView() {
+
+    final size =MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+    final swiperHeight = width / 16 * 9;
+    print(width);
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: swiperHeight,
+      child: Swiper(
+        itemCount: 4,
+        itemBuilder: _swiperBuilder,
+        pagination: new SwiperPagination(
+            alignment: Alignment.bottomCenter,
+            margin: const EdgeInsets.fromLTRB(0, 0, 20, 10),
+            builder: DotSwiperPaginationBuilder(
+                color: Colors.black54,
+                activeColor: Colors.white
+            )
+        ),
+        controller: SwiperController(),
+        scrollDirection: Axis.horizontal,
+        autoplay: true,
+        onTap: (index) => print('点击了第$index'),
+      ),
+    );
+  }
+
+  Widget _swiperBuilder(BuildContext context, int index) {
+    return (imageList[index]);
+  }
+
+}
--- lib/home/recommend/index.dart
+++ lib/home/recommend/index.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
+import '../../components/swiper.dart';
 
 class HomeRecommend extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body: new Container(
-        color: Colors.blue[50],
-        child: Align(
-          widthFactor: 4,
-          heightFactor: 4,
-          alignment: Alignment(1, -1),
-          child: new Container(
-            width: 30,
-            height: 30,
-            color: Colors.green
+      body: new Column(
+        children: <Widget>[
+          new Container(
+            child: new SwiperView(),
           ),
-        ),
-      )
+          new Container(
+            height: 40,
+            color: Colors.yellow,
+          ),
+          new Container(
+            height: 40,
+            color: Colors.black,
+          )
+        ],
+      ),
     );
   }
 }
