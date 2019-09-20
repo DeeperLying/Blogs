d58742ecb10e3cdfc7cf455875f3663e0d568852
Deeper-Lee <984130957@qq.com>
2019-09-20 18:32:46 +0800

    加入状态管理redux

---
+++ lib/redux/user.dart
@@ -0,0 +1,21 @@
+/*
+import 'package:meta/meta.dart';
+
+class User { // 用于定义存储内容
+  String _name;
+  get name => _name;
+  User(this._name);
+}
+
+enum Action {
+  increment
+}
+
+ContState reducer(ContState state, action){
+  // 匹配Action
+  if(action == Action.increment){
+  return CountState(state.count+1);
+  }
+  return state;
+}
+*/
